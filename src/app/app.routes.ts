c37b89e11ba1045a1b79943753e1f5279b39a5f5
HaruYQing <haru61410@gmail.com>
2026-02-12 09:19:46 +0800

    core: routes settings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Layout } from './core/layout/layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Layout,
+    children: [
+      {
+        path: 'playground',
+        loadComponent: () => import('./features/playground/playground').then((m) => m.Playground),
+      },
+    ],
+  },
+];
